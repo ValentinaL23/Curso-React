38bcbfc983c72fe58918fab03829339d5821af41
ValentinaL23 <dayana.largo.p@gmail.com>
2023-04-15 23:44:08 -0500

    CrsReact_1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+export function App () {
+  return (
+    <>
+      <TwitterFollowCard userName="panchi" name="Pandi Mochi" />
+      <TwitterFollowCard userName="camith" name="Camila Smith" />
+      <TwitterFollowCard userName="gummibeer" name="Pandi Mochi" />
+    </>
+  )
+}
