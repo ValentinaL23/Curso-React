b0dd6948f7b2a0ff3bb0cef7685948d678d32f5a
ValentinaL23 <dayana.largo.p@gmail.com>
2023-04-16 01:53:08 -0500

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import './index.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App () {
   return (
-    <>
-      <TwitterFollowCard userName="panchi" name="Pandi Mochi" />
-      <TwitterFollowCard userName="camith" name="Camila Smith" />
-      <TwitterFollowCard userName="gummibeer" name="Pandi Mochi" />
-    </>
+    <section className="App">
+      <TwitterFollowCard isFollowing={false} userName="panchi" name="Pandi Mochi" />
+      <TwitterFollowCard isFollowing userName="camith" name="Camila Smith" />
+      <TwitterFollowCard isFollowing={false} userName="gummibeer" name="Pandi Mochi" />
+    </section>
   )
 }
